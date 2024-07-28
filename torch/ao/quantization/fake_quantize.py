2c8644e48e806fa709503aa68e0bc1a465640c47
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2024-07-28 03:29:32 +0000

    Revert "[BE] typing for decorators - _jit_internal (#131573)"
    This reverts commit f0f20f7e97716b4b077dca2a1a42930ccf990c1c.
    
    Reverted https://github.com/pytorch/pytorch/pull/131573 on behalf of https://github.com/clee2000 due to breaking lint internally D60265575 ([comment](https://github.com/pytorch/pytorch/pull/131572#issuecomment-2254328359))
    
    (cherry picked from commit 609447a626fb014a84dfd384d40c121ea8bde825)

--- torch/ao/quantization/fake_quantize.py
+++ torch/ao/quantization/fake_quantize.py
@@ -1,3 +1,4 @@
+# mypy: allow-untyped-decorators
 # mypy: allow-untyped-defs
 """Implements modules  used to perform fake quantization."""
 
@@ -327,7 +328,7 @@ class FusedMovingAvgObsFakeQuantize(FakeQuantize):
         self.is_symmetric_quant = _is_symmetric_quant(self.activation_post_process.qscheme)
 
     @torch.jit.export
-    def calculate_qparams(self) -> Tuple[torch.Tensor, torch.Tensor]:  # type: ignore[override]
+    def calculate_qparams(self) -> Tuple[torch.Tensor, torch.Tensor]:
         return self.activation_post_process.calculate_qparams()
 
     @torch.jit.export
